299973fa32f3b0950bd022512c7c09ae051314a2
Ron Nuss <shalom@itsron.space>
2023-08-01 13:00:40 +0300

    Updated version and fixed issue

---
+++ build/todayNews/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Retrieves the news titles from "Hadshot Hayom" and returns them in a JSON format.
+ *
+ * @returns {Promise<string[] | null>} An array of news titles extracted from the website or null if an error occurs.
+ */
+export declare function todayNews(): Promise<string[] | null>;
